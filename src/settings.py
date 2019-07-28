e05adaa47df2b169ac7744920a51509f20ecb8be
Leo Covarrubias <leo.covarrubias@gmail.com>
2019-07-27 21:42:05 -0400

    more env vars, update readme

--- src/settings.py
+++ src/settings.py
@@ -6,12 +6,13 @@ env.read_env()
 
 
 class Config:
-    MAX_YEARS = 10
+    MAX_YEARS = env.int('MAX_YEARS', 10)
     MAX_FILE_NAME_LEN = 80
     OAUTH_TOKEN = env('OAUTH_TOKEN')
     API_URL = 'https://www.tadpoles.com/remote/v1'
     EVENTS_URL = f'{API_URL}/events'
     ATTACHMENTS_URL = f'{API_URL}/obj_attachment'
+    SKIP_NO_DATA_CHECK = env.bool('SKIP_NO_DATA_CHECK', False)
 
 
 def get_client():
