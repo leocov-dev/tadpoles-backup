ebf6a9247f0f5ceeaf2c0d6ce89e065b7b2bff46
Leo Covarrubias <leo@leocov.com>
2020-03-14 21:15:04 -0400

    exif functional

--- internal/utils/misc.go
+++ internal/utils/misc.go
@@ -3,8 +3,13 @@ package utils
 import (
 	"fmt"
 	"github.com/briandowns/spinner"
+	"github.com/h2non/filetype/matchers"
+	"github.com/h2non/filetype/types"
+	"github.com/leocov-dev/tadpoles-backup/config"
 	"io"
 	"os"
+	"path"
+	"strings"
 	"time"
 )
 
@@ -56,5 +61,41 @@ func MoveFile(sourcePath, destPath string) error {
 	if err != nil {
 		return fmt.Errorf("failed removing original file: %s", err)
 	}
+	return nil
+}
+
+func IsImageType(t types.Type) bool {
+	for k := range matchers.Image {
+		if k == t {
+			return true
+		}
+	}
+	return false
+}
+
+func CleanupTempFiles() (err error) {
+
+	tempDir := os.TempDir()
+
+	f, err := os.Open(tempDir)
+	if err != nil {
+		return err
+	}
+	defer CloseWithLog(f)
+
+	files, err := f.Readdir(0)
+	if err != nil {
+		return err
+	}
+
+	for _, file := range files {
+		if strings.HasPrefix(file.Name(), strings.TrimSuffix(config.TempFilePattern, "*")) {
+			err = os.Remove(path.Join(tempDir, file.Name()))
+			if err != nil {
+				return err
+			}
+		}
+	}
+
 	return nil
 }
