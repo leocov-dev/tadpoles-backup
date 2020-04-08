7fe600c9b00ddc086d0ba46ae71f7c39087faafe
Leo Covarrubias <leo@leocov.com>
2020-04-07 21:13:07 -0400

    travis config

--- scripts/travis_prerelease.sh
+++ scripts/travis_prerelease.sh
@@ -24,6 +24,7 @@ if [[ -n "${TRAVIS_BRANCH}" &&
     git config --global user.name "Travis CI"
     git config --global user.email "builds@travis-ci.com"
     git tag -fa "$TRAVIS_TAG" -m "Prerelease ${TRAVIS_TAG}"
+    git push -f --tags
 
     echo
 fi
