9d299c61ac07798c7c8f38fd978a1c44e86406b8
Leonardo Covarrubias <leo@leocov.com>
2023-08-15 12:55:42 -0400

    chore: comments

--- internal/schemas/download_pool.go
+++ internal/schemas/download_pool.go
@@ -21,17 +21,30 @@ func NewDownloadPool(concurrencyLimit int) *DownloadPool {
 	return dpl
 }
 
+// Add
+// put a new attachment download request into the download queue
 func (dpl *DownloadPool) Add(proc *AttachmentProc) {
 	dpl.queue <- proc
 }
 
+// init
+// build out the download workers and set them listening to the queue
 func (dpl *DownloadPool) init() {
+	// make a number of worker goroutines
 	for worker := 0; worker < dpl.maxWorkers; worker++ {
+		// each worker is a wait group
 		dpl.wg.Add(1)
 
+		// start a new goroutine
 		go func() {
+			// if the worker has no more to do (function has exited)
+			// then mark its wait group as done
 			defer dpl.wg.Done()
 
+			// "ranging" a channel is a blocking call that ticks when new
+			// data is pushed on the channel.
+			// This allows each worker to listen to the queue channel, pull
+			// a download request from it, and execute the request.
 			for proc := range dpl.queue {
 				proc.Execute()
 			}
@@ -40,6 +53,11 @@ func (dpl *DownloadPool) init() {
 }
 
 func (dpl *DownloadPool) Process() {
+	// since our channel does not specify a length we need to close it
+	// this will make it so that the workers don't wait for more data
+	// once the current data in the queue is exhausted
 	close(dpl.queue)
+
+	// wait for all the wait groups to be done (all worker goroutines have exited)
 	dpl.wg.Wait()
 }
